6a16f33861c5daab40fd927ed3001a1e7249151d
Ron <lucifron28@gmail.com>
2025-05-20 01:17:04 +0800

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
