1a5098ae9975f7ff8a2d1e4f2b7aab47f5d1b0c0
Ron <lucifron28@gmail.com>
2025-05-20 17:02:03 +0800

    base functionality

--- lib/bmi_calculator.dart
+++ lib/bmi_calculator.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/app.dart';
 import 'package:flutter/material.dart';
 import 'unit_text_form_field.dart';
 
@@ -11,12 +12,13 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   TextEditingController heightController = TextEditingController();
   TextEditingController weightController = TextEditingController();
+  String resultBMI = "Calculate your BMI";
 
   String? selectedWeightUnit = "kg";
   List<String> weightUnits = ["kg", "lbs"];
 
   String? selectedHeightUnit = "cm";
-  List<String> heightUnits = ["cm", "inch"];
+  List<String> heightUnits = ["cm", "in", "m"];
   bool isNumeric(String s) {
     if (s.isEmpty) {
       return false;
@@ -24,39 +26,38 @@ class _CalculatorState extends State<Calculator> {
     return double.tryParse(s) != null;
   }
 
+double calculateBMI(double height, double weight) {
+  if (selectedHeightUnit == "cm") {
+    height = height / 100;
+  } else if (selectedHeightUnit == "in") {
+    height = height * 0.0254;
+  } else if (selectedHeightUnit == "m") {
+  }
+
+  if (selectedWeightUnit == "lbs") {
+    weight = weight * 0.453592;
+  }
+
+  return weight / (height * height);
+}
+
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Text("Your BMI: "),
+        Text(resultBMI),
         Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               UnitTextFormField(
-                controller: heightController,
+                controller: weightController,
                 labelText: "Enter Weight",
-                selectedUnit: selectedHeightUnit,
-                units: heightUnits,
-                onUnitChanged: (value) {
-                  setState(() {
-                    selectedHeightUnit = value;
-                  });
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please Enter a Number";
-                  }
-                  if (!isNumeric(value)) {
-                    return "Invalid number format";
-                  }
-                },
-              ),
-              SizedBox(height: 20),
-              UnitTextFormField(
-                controller: heightController,
-                labelText: "Enter Height",
                 selectedUnit: selectedWeightUnit,
                 units: weightUnits,
                 onUnitChanged: (value) {
@@ -73,15 +74,42 @@ class _CalculatorState extends State<Calculator> {
                   }
                 },
               ),
+              SizedBox(height: 20),
+              UnitTextFormField(
+                controller: heightController,
+                labelText: "Enter Height",
+                selectedUnit: selectedHeightUnit,
+                units: heightUnits,
+                onUnitChanged: (value) {
+                  setState(() {
+                    selectedHeightUnit = value;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please Enter a Number";
+                  }
+                  if (!isNumeric(value)) {
+                    return "Invalid number format";
+                  }
+                  return null;
+                },
+              ),
             ],
           ),
         ),
         ElevatedButton(
           onPressed: () {
-
-          }, 
+            double bmi = calculateBMI(
+              double.parse(heightController.text.trim()),
+              double.parse(weightController.text.trim()),
+            );
+            setState(() {
+              resultBMI = bmi.toString();
+            });
+          },
           child: Text("Submit"),
-          ),
+        ),
       ],
     );
   }
