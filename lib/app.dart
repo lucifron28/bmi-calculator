cf9f5a9cd263d19d265551adaf097ed8f12e5ce6
Ron <lucifron28@gmail.com>
2025-05-20 11:22:31 +0800

    modularized

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bmi_calculator.dart';
 
 
 class BMICalculatorApp extends StatelessWidget {
@@ -6,6 +7,25 @@ class BMICalculatorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BMI Calculator",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+        centerTitle: true,
+      ),
+      body: Calculator(),
+    );
   }
 }
